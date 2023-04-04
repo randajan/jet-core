ce28972f8e2f4616c71729b69d1d174fe46f5711
Jan Randa <randajan@email.cz>
2023-04-04 23:16:52 +0200

    BF: enumFactory

--- src/jet/pile.js
+++ src/jet/pile.js
@@ -166,6 +166,7 @@ export const vault = name=>{
 export const enumFactory = (enums, {before, after, def}={})=>(raw, ...args)=>{
     const input = before ? before(raw, ...args) : raw;
     const output = enums.includes(input) ? input : def;
+    
     return after ? after(output, ...args) : output;
 }
 
