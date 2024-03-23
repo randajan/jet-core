833d55a5a502f1b8e38bb3a138bd43e983cb0fe3
Jan Randa <jnranda@gmail.com>
2024-03-23 11:30:04 +0100

    ADD: jet.buffer add argument maxQueueMs

--- src/jet/buffer.js
+++ src/jet/buffer.js
@@ -1,23 +1,30 @@
 
 
 
-export const buffer = (processQueue, bufferMs=100, maxQueueSize=100)=>{
+export const buffer = (processQueue, bufferMs=100, maxQueueMs=0, maxQueueSize=0)=>{
     if (!jet.isRunnable(processQueue)) { throw new Error("jet.buffer(...) require function as a first argument"); }
     bufferMs = Math.max(0, Number.jet.to(bufferMs));
+    maxQueueMs = Math.max(0, Number.jet.to(maxQueueMs));
     maxQueueSize = Math.max(0, Number.jet.to(maxQueueSize));
 
-    let int, queue = [];
+    let intA, intB, queue = [];
 
     const execute = _=>{
+        clearTimeout(intA);
+        clearTimeout(intB);
         const q = queue;
         queue = [];
         processQueue(q);
     };
+    
 
     return (...args)=>{
-        clearTimeout(int);
+        clearTimeout(intA);
         if (maxQueueSize && queue.length >= maxQueueSize) { return execute(); }
         queue.push(args);
-        int = setTimeout(execute, bufferMs);
+        intA = setTimeout(execute, bufferMs);
+        if (maxQueueMs > bufferMs && queue.length === 1) {
+            intB = setTimeout(execute, maxQueueMs);
+        }
     }
 }
