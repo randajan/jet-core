89679fe362d7b83dd391e35c2337549eef36dca0
Jan Randa <jnranda@gmail.com>
2024-04-05 00:26:00 +0200

    RM: jet.buffer and moved to separate library @randajan/queue

--- src/jet/buffer.js
+++ src/jet/buffer.js
@@ -1,30 +1,6 @@
 import jet from "./defs";
 
 
-export const buffer = (processQueue, bufferMs=100, maxQueueMs=0, maxQueueSize=0)=>{
-    if (!jet.isRunnable(processQueue)) { throw new Error("jet.buffer(...) require function as a first argument"); }
-    bufferMs = Math.max(0, Number.jet.to(bufferMs));
-    maxQueueMs = Math.max(0, Number.jet.to(maxQueueMs));
-    maxQueueSize = Math.max(0, Number.jet.to(maxQueueSize));
-
-    let intA, intB, queue = [];
-
-    const execute = _=>{
-        clearTimeout(intA);
-        clearTimeout(intB);
-        const q = queue;
-        queue = [];
-        processQueue(q);
-    };
-    
-
-    return (...args)=>{
-        clearTimeout(intA);
-        if (maxQueueSize && queue.length >= maxQueueSize) { return execute(); }
-        queue.push(args);
-        intA = setTimeout(execute, bufferMs);
-        if (maxQueueMs > bufferMs && queue.length === 1) {
-            intB = setTimeout(execute, maxQueueMs);
-        }
-    }
+export const buffer = _=>{
+    throw Error("jet.buffer(...) was moved to separate library @randajan/queue");
 }
