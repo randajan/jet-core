0b80381013c22a77e937478016f61b78a0cd53b8
Jan Randa <jnranda@gmail.com>
2024-04-21 19:41:07 +0200

    ADD: each sync now working with filter and orderBy, next step: ASYNC version :)

--- demo/src/index.js
+++ demo/src/index.js
@@ -4,9 +4,13 @@ import example from "./example.json";
 window.jet = jet;
 
 
-const result = jet.remap(example, (v, ctx)=>{
-    return v.name;
-}, { deep:false, orderBy:[ v=>v.name ] });
+const result = jet.remap({a:3, z:2, f:0, d:9, q:1}, (v, ctx)=>{
+    console.log(v);
+    return v;
+}, {
+    strictArray:true,
+    orderBy:[(v, k)=>k, false]
+});
 
 
 console.log(result);
--- src/jet/each.js
+++ src/jet/each.js
@@ -42,23 +42,78 @@ const createContext = (parent, key, value)=>{
         isRoot:{enumerable, value:false},
         root:{value:parent.root},
         parent:{value:parent},
-        key:{enumerable, value:key},
-        value:{enumerable, value},
+        key:{enumerable, get:_=>key, set:k=>{ key = k; fullpath = undefined; }},
+        value:{enumerable, get:_=>value, set:v=>{ value = v; def = undefined; }},
         depth:{enumerable, value:parent.depth+1},
         pending:{enumerable, get:_=>parent.pending},
         stop:{enumerable, value:parent.stop},
         path:{enumerable, get:_=>[...(path || (path = collectPath(parent)))]},
         fullpath:{enumerable, get:_=>fullpath || (fullpath = [...ctx.path, key]) },
         def:{enumerable, get:_=>def || (def = getDefByInst(value))},
+        update:{enumerable, value:(...a)=>{
+            if (a.length > 0) { ctx.value = a[0]; }
+            if (a.length > 1) { ctx.key = a[1]; }
+            return ctx;
+        }}
     });
 
     return ctx;
 }
 
-const _each = (any, parent, exe)=>{
-    for (let [key, val] of parent.def.entries(any)) {
+const _fight = (a, b)=>{
+    if (a == b) { return; }
+    if (a == null) { return true; } else if (b == null) { return false; }
+    if (typeof a === "string" || typeof b === "string") { return a === String.jet.fight(a, b); }
+    if (isNaN(a)) { return true; } else if (isNaN(b)) { return false; }
+    return a < b;
+}
+
+const _filter = (entries, fce)=>{
+    const res = [];
+    for (let en of entries) {
+        if (fce(en[1], en[0])) { res.push(en); }
+    }
+    return res;
+}
+
+const formatOrderBy = (orderBy)=>{
+    if (!Array.isArray(orderBy)) { return [[orderBy, true]]; }
+    const obs = [];
+    for (let i=0; i<orderBy.length; i++) {
+        const ob = orderBy[i];
+        if (Array.isArray(ob)) { obs.push(ob); continue; }
+        if (!jet.isRunnable(ob)) { continue; }
+        const asc = (typeof orderBy[i+1] === "boolean") ? orderBy[i = i+1] : true;
+        obs.push([ob, asc]);
+    }
+    return obs;
+}
+
+const _refine = (entries, { filter, orderBy })=>{
+    if (!filter && !orderBy) { return entries; }
+    entries = filter ? _filter(entries, filter) : [...entries];
+
+    if (!orderBy) { return entries; }
+
+    const obs = formatOrderBy(orderBy);
+    const expand = entries.map(([key, value])=>[key, value, obs.map(ob=>ob[0](value, key))]);
+
+    return expand.sort(([aK, aV, aO], [bK, bV, bO]) => {
+        for (const k in obs) {
+            const dir = _fight(aO[k], bO[k]);
+            if (dir == null) { continue; }
+            const asc = obs[k][1];
+            return (asc == null ? !dir : dir != asc) * 2 - 1;
+        }
+        return 0;
+    });
+
+}
+
+const _each = (entries, parent, exe)=>{
+    for (let [key, val] of entries) {
         const ctx = createContext(parent, key, val);
-        if (ctx.pending) { exe(val, ctx); }
+        if (ctx.pending) { exe(ctx); }
         if (!ctx.pending) { break; }
     };
 }
@@ -66,18 +121,18 @@ const _each = (any, parent, exe)=>{
 export const each = (any, fce, options={})=>{
     if (!jet.isRunnable(fce)) { throw new Error(`fce expect an Function`); }
     const root = initContext(any, options.root);
-
+    
     const deep = options.deep;
     const dprun = jet.isRunnable(deep);
 
-    const exe = (val, ctx, isRoot)=>{
-        const dp = ctx.def.entries;
-        if (dp && dprun) { deep(val, ctx, _=>_each(val, ctx, exe)); }
-        else if (dp && (deep || isRoot)) { _each(val, ctx, exe); }
-        else { fce(val, ctx); }
+    const exe = (ctx, skipDeep=false)=>{
+        const de = ctx.def.entries;
+        if (!de || (!deep && !ctx.isRoot)) { fce(ctx.value, ctx); }
+        else if (dprun && !skipDeep) { deep(ctx.value, ctx, (...a)=>exe(ctx.update(...a), true)); }
+        else { _each(ctx.isRoot ? _refine(de(ctx.value), options) : de(ctx.value), ctx, exe); }
     }
     
-    exe(any, root, true);
+    exe(root, true);
 };
 
 
@@ -123,7 +178,10 @@ export const remap = (any, fce, options={})=>{
             set(ctx.parent, ctx.key, ctx.result = ctx.def.create());
             if (!result && ctx.isRoot) { result = ctx.result; }
         }
-        ctx.def.set(ctx.result, key, val);
+
+        if (!options.strictArray && ctx.def.name === "Array") { ctx.result.push(val); }
+        else { ctx.def.set(ctx.result, key, val); }
+        
     }
 
     each(any, (val, ctx)=>{
